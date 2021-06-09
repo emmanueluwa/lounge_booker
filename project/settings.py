98300c004d09c493eb47077125c65f1816741742
emmanueluwa <fulo1@gmail.com>
2021-06-09 16:15:04 +0100

    Adding models and installing app

--- project/settings.py
+++ project/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "lounge_booker.apps.LoungeBookerConfig",
 ]
 
 MIDDLEWARE = [
@@ -78,7 +79,7 @@ DATABASES = {
         "ENGINE": "django.db.backends.postgresql_psycopg2",
         "NAME": "lounge_booker_db",
         "USER": "postgres",
-        "PASSWORD": "r4$AMGexn9a#4mJd",
+        "PASSWORD": "admin",
         "HOST": "localhost",
         "PORT": "5432",
     }
