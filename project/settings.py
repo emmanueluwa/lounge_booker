c1a8f2660f612ccb1436f3cef2bf3afc32c815e6
emmanueluwa <fulo1@gmail.com>
2021-06-09 13:54:11 +0100

    adding postgres database engine

--- project/settings.py
+++ project/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": "lounge_booker_db",
+        "USER": "postgres",
+        "PASSWORD": "r4$AMGexn9a#4mJd",
+        "HOST": "localhost",
+        "PORT": "5432",
     }
 }
 
