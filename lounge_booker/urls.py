406ccfb21eaf284fefa67310c9399fd3ee840568
emmanueluwa <fulo1@gmail.com>
2021-06-12 12:03:45 +0100

    homepage with authentication

--- lounge_booker/urls.py
+++ lounge_booker/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 
 from . import views 
 
-# app_name = lounge_booker
+app_name = "lounge_booker"
 
 urlpatterns = [
     path("", views.home_page, name="home"),
--- lounge_booker/views.py
+++ lounge_booker/views.py
@@ -1,10 +1,15 @@
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.forms import AuthenticationForm
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Lounge
 
 def home_page(request):
-    return render(request, "home.html", context={})
+    if not request.user.is_authenticated:
+        return redirect("lounge_booker:login")
+    
+    context = {"lounges": Lounge.objects.all()}
+    return render(request, "home.html", context=context)
 
 
 def login_page(request):
