67466d2c550c37d8b1454af875df2448c8ec7fad
emmanueluwa <fulo1@gmail.com>
2021-06-14 15:29:57 +0100

    testing the login page

--- lounge_booker/tests.py
+++ lounge_booker/tests.py
@@ -8,6 +8,7 @@ from django.test import TestCase
 from .factories import LoungeFactory, LoungeBookFactory, UserFactory
 from .forms import BookingForm
 from .models import Lounge
+from django.contrib.auth.forms import AuthenticationForm
 
 
 class HomePageTests(TestCase):
@@ -28,8 +29,7 @@ class HomePageTests(TestCase):
 
         self.assertEqual(list(context), list(Lounge.objects.all()))
 
-    
-    
+      
     def test_template_rendered(self):
         self.client.force_login(self.user)
         response = self.client.get("/")
@@ -38,3 +38,43 @@ class HomePageTests(TestCase):
         self.assertTemplateUsed(response, "home.html")
 
 
+class LoginPageTests(TestCase):
+    def setUp(self):
+        self.user = UserFactory()
+        self.url = "/login"
+        self.response = self.client.get(self.url)
+
+    
+    def test_blank_login_page(self):
+        context_form = self.response.context["login_form"]
+
+        self.assertEqual(self.response.status_code, 200)
+        self.assertIsInstance(context_form, AuthenticationForm)
+        self.assertTemplateUsed(self.response, "login.html")
+
+
+    def test_successful_login(self):
+            data = {
+                "username": self.user.username,
+                "password": "top-secret",
+            }
+            response = self.client.post(self.url, data, follow=True)
+
+            message = list(response.context.get("messages"))[0]
+            self.assertEqual(message.tags, "info")
+            self.assertTrue(
+                f"Hello {self.user.username}, you are now logged in." in message.message
+            )
+            self.assertRedirects(response, "/", status_code=302)
+
+
+    def test_unsuccessful_login(self):
+        data = {
+            "username": self.user.username,
+            "password": "wrong-password",
+        }
+        response = self.client.post(self.url, data, follow=True)
+
+        message = list(response.context.get("messages"))[0]
+        self.assertEqual(message.tags, "error")
+        self.assertTrue("Invalid username or password, please try again." in message.message)
